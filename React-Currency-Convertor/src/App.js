0856ccbdd1773f912b24ae2b39724e673a413543
Rashi Jaiswal <rashijaiswal1405@gmail.com>
2023-02-06 23:27:57 +0530

    Add files via upload

---
+++ React-Currency-Convertor/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Convertor from './CurrencyConverter';
+function App() {
+  return (
+    <div className="App">
+      <Convertor />
+    </div>
+  );
+}
+
+export default App;
